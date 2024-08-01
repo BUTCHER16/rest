973d54956a07441d75608c5c9eca531e6b2cdbaa
BUTCHER16 <ravidossalbert@gmail.com>
2024-08-01 09:52:59 +0530

    Learned about the end point and How to get the responsce from the Url to print it out to the terminal and also about the Json and how it plays the role in the django rest Framework

---
+++ py_client/basic.py
@@ -0,0 +1,47 @@
+import requests
+
+endpoint = 'https://httpbin.org/status/200'
+endpoint = 'https://httpbin.org/anything'
+
+#get_responsce = requests.get(endpoint) # Application Programming interface
+
+# we can also pass in our own json data to the requests, if you want to use json we can just think of an python Dict 
+get_responsce = requests.get(endpoint, json = {'query':'Hello World'})
+# it will just echo back the dict that we pass
+# we can also send it as an raw data just replace 'json' with 'data' it will consider as a form in the outPut
+
+print(get_responsce.text)
+
+# where we can also get the json 
+print(get_responsce.json())
+
+# there's also a status code that is used to know the out like error code ( 404 like this )
+
+
+# Rest Api -> is for an Web based Api it uses the HTTP Requests
+# This is how we can get the python to scrap the web andget the content from the web
+
+# HTTP Request ->  gives the HTML content
+# But the REST API Request which is still ( HTTP ) REST API HTTP Request -> this gives Json(xml) responsce
+
+
+"""{
+  "args": {},
+  "data": "",
+  "files": {},
+  "form": {},
+  "headers": {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate",
+    "Host": "httpbin.org",
+    "User-Agent": "python-requests/2.32.3",
+    "X-Amzn-Trace-Id": "Root=1-66ab0584-56b75c417a9130a77bb41e50"
+  },
+  "json": null,
+  "method": "GET",
+  "origin": "106.51.127.45",
+  "url": "https://httpbin.org/anything"
+}"""
+
+# This is just like the python Dict but its not 
+# JavaScript Object Notation
